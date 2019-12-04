f2b0c4d24056d669d01282b520ebbf73c6a28bb8
efenko <evgeny@efenko.1plus1.corp>
2019-12-04 15:16:51 +0200

    update

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+    public interface CanFly {
+        public void method1();
+    }
+
+    public interface CanRun {
+        public void method1();
+    }
+
+    public interface CanSwim {
+        public void method1();
+    }
+
+}
