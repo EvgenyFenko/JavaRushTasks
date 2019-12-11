fd48a0617d6908ffe81aae1f3bb772db3c445793
efenko <evgeny@efenko.1plus1.corp>
2019-12-11 20:32:59 +0200

    1524 in progress

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    private Solution(int n){
+
+    }
+
+    public Solution() {
+    }
+
+    Solution(String s) {
+
+    }
+
+    protected Solution(int n, int m) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+    protected SubSolution(int n) {
+    }
+
+    public SubSolution(int n, int m) {
+
+    }
+
+    SubSolution(String s) {
+
+    }
+
+
+}
