d85a70ce51006b4ec0a8d67ee6d588e0e162f9ef
efenko <evgeny@efenko.1plus1.corp>
2019-12-23 21:27:09 +0200

    1822 done

--- 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -5,34 +5,28 @@ package com.javarush.task.task18.task1822;
 */
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        int id = 21;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String file = reader.readLine();
-        reader.close();
+        FileReader fileReader = new FileReader(reader.readLine());
+        BufferedReader fileBufReader = new BufferedReader(fileReader);
 
-//        String file = "/home/evgeny/file1.txt";
+        String res;
 
-        Scanner scanner = new Scanner(new File(file));
-        ArrayList<String> arrayList = new ArrayList<>();
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            String idInLine = (line.split(" "))[0];
-            if (Integer.parseInt(idInLine) == id) {
-                System.out.println(line.toString());
+        while ((res = fileBufReader.readLine()) != null) {
+            if (res.startsWith(args[0] + " ")) {
+                System.out.println(res);
+                break;
             }
-            scanner.close();
-//            arrayList.add(line[0]);
-//            arrayList.add(scanner.nextLine());
-//            arrayList.add(line.split(" "));
         }
-//        System.out.println(arrayList.toString());
+
+        reader.close();
+        fileBufReader.close();
+        fileBufReader.close();
+
     }
 }
