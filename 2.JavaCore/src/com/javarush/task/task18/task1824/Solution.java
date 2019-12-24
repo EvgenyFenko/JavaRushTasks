7ff54f3a86827779ce982cacca7783eb0f8b4c01
efenko <evgeny@efenko.1plus1.corp>
2019-12-24 21:37:43 +0200

    1902 done

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task18.task1824;
+
+/* 
+Файлы и исключения
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String fileName = consoleReader.readLine();
+            try {
+                BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+                fileReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                consoleReader.close();
+                break;
+            }
+        }
+    }
+}
