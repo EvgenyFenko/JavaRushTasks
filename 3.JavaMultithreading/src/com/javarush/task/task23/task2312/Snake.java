8d9c01d58b43eef99fd4e017825694bd3328b86a
efenko <evgeny@efenko.1plus1.corp>
2019-12-26 10:55:55 +0200

    2312 in progress - part 8 done; clean file out.log

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
@@ -23,5 +24,25 @@ public class Snake {
         this.direction = direction;
     }
 
+    public Snake(int x, int y) {
+        SnakeSection head = new SnakeSection(x, y);
+        sections = new ArrayList<>();
+        this.sections.add(head);
+        this.isAlive = true;
+    }
+
+    public int getX() {
+        SnakeSection head = sections.get(0);
+        return head.getX();
+    }
+
+    public int getY() {
+        SnakeSection head = sections.get(0);
+        return head.getY();
+    }
+
+    public void move() {
+
+    }
 
 }
