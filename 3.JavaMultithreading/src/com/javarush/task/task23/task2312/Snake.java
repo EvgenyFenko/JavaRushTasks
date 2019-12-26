f9ddad9c319ba063de412ab60ed27c35b7270343
efenko <evgeny@efenko.1plus1.corp>
2019-12-26 20:25:31 +0200

    2312 all Tasd done

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -8,27 +8,11 @@ public class Snake {
     private boolean isAlive;
     private SnakeDirection direction;
 
-    public List<SnakeSection> getSections() {
-        return sections;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public SnakeDirection getDirection() {
-        return direction;
-    }
-
-    public void setDirection(SnakeDirection direction) {
-        this.direction = direction;
-    }
-
     public Snake(int x, int y) {
         SnakeSection head = new SnakeSection(x, y);
         sections = new ArrayList<>();
-        this.sections.add(head);
-        this.isAlive = true;
+        sections.add(head);
+        isAlive = true;
     }
 
     public int getX() {
@@ -42,7 +26,75 @@ public class Snake {
     }
 
     public void move() {
-
+        if (isAlive){
+            if (direction==SnakeDirection.UP){
+                move(0,-1);
+            } else if (direction==SnakeDirection.RIGHT){
+                move(1,0);
+            } else if (direction==SnakeDirection.DOWN){
+                move(0,1);
+            } else if (direction==SnakeDirection.LEFT){
+                move(-1,0);
+            }
+        }
     }
 
+    public void move(int a, int b) {
+        //Создаем новую голову - новый "кусочек змеи".
+        SnakeSection head = sections.get(0);
+        head = new SnakeSection(head.getX() + a, head.getY() + b);
+
+        //Проверяем - не вылезла ли голова за границу комнаты
+        checkBorders(head);
+        if (!isAlive) return;
+
+        //Проверяем - не пересекает ли змея  саму себя
+        checkBody(head);
+        if (!isAlive) return;
+
+        //Проверяем - не съела ли змея мышь.
+        Mouse mouse = Room.game.getMouse();
+        if (head.getX() == mouse.getX() && head.getY() == mouse.getY()) //съела
+        {
+            sections.add(0, head);                  //Добавили новую голову
+            Room.game.eatMouse();                   //Хвот не удаляем, но создаем новую мышь.
+        } else //просто движется
+        {
+            sections.add(0, head);                  //добавили новую голову
+            sections.remove(sections.size() - 1);   //удалили последний элемент с хвоста
+        }
+    }
+
+    public void checkBorders (SnakeSection head){
+        int headX = head.getX();
+        int headY = head.getY();
+        int widthRoom = Room.game.getWidth();
+        int heightRoom = Room.game.getHeight();
+
+        if (headX<0 || headX>=widthRoom || headY<0 || headY>=heightRoom){
+            isAlive = false;
+        }
+        System.out.println(isAlive);
+    }
+    public void checkBody(SnakeSection head){
+        if (sections.contains(head)){
+            isAlive=false;
+        }
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
 }
