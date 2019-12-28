621f027fd731ed4ba2a4585d5117b2e92da950e6
unknown <fenko.ievgenii@gmail.com>
2019-12-28 22:44:36 +0200

    2313 done

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,25 +1,18 @@
 package com.javarush.task.task24.task2413;
 
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
+    //координаты
     protected double x;
     protected double y;
+    //радиус объекта
     protected double radius;
 
-    public BaseObject(double x, double y, double radius) {
+    protected BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
-        this.radius = radius;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public void setRadius(double radius) {
         this.radius = radius;
     }
 
@@ -27,25 +20,54 @@ public abstract class BaseObject {
         return x;
     }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
     public double getY() {
         return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
     }
 
     public double getRadius() {
         return radius;
     }
 
-    public abstract void  draw(Canvas canvas);
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 
-    public abstract void move();
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    abstract void draw(Canvas canvas);
 
-    public boolean isIntersec(BaseObject o) {
-        double distanse;
-        double diffX;
-        double diffY;
-        diffX = this.getX() - o.getX();
-        diffY = this.getY() - o.getY();
-        distanse = Math.sqrt(diffX*diffX + diffY*diffY);
-        return distanse <= Math.max(this.radius, o.radius);
+    /**
+     * Двигаем себя на один ход.
+     */
+    abstract void move();
+
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    boolean isIntersec(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
     }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,33 +1,14 @@
 package com.javarush.task.task24.task2413;
 
+/**
+ * Класс-холст для отрисовки.
+ */
 public class Canvas {
+    //ширина и высота
     private int width;
     private int height;
+    //матрица, где рисуем. символ - это цвет.
     private char[][] matrix;
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setMatrix(char[][] matrix) {
-        this.matrix = matrix;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public char[][] getMatrix() {
-        return matrix;
-    }
 
     public Canvas(int width, int height) {
         this.width = width;
@@ -35,16 +16,17 @@ public class Canvas {
         this.matrix = new char[height + 2][width + 2];
     }
 
-    void setPoint(double x, double y, char c) {
-
-        int intX = (int) Math.round(x);
-        int intY = (int) Math.round(y);
-
-        if(!(intX < 0 || intY < 0 || intY >= matrix.length || intX >= matrix[0].length)) {
-            matrix[intY][intX] = c;
-        }
+    /**
+     * Очищаем холст
+     */
+    void clear() {
+        this.matrix = new char[height + 2][width + 2];
     }
 
+    /**
+     * Печатаем переданную фигуру в указанных координатах цветом c.
+     * Если переданный массив содержит единицы, то на холсте им будут соответствовать символы - с.
+     */
     void drawMatrix(double x, double y, int[][] matrix, char c) {
         int height = matrix.length;
         int width = matrix[0].length;
@@ -57,29 +39,48 @@ public class Canvas {
         }
     }
 
-    public void clear() {
-//        int height = matrix.length;
-//        int width = matrix[0].length;
-//
-//        for (int i = 0; i < height; i++) {
-//            for (int j = 0; j < width; j++){
-//                matrix[i][j] = ' ';
-//            }
-//        }
-        matrix = new char[height + 2][width + 2];
+    /**
+     * Ставим одну точку на холсте с координатами (x,y) и цветом - c.
+     */
+    void setPoint(double x, double y, char c) {
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+        if (y0 < 0 || y0 >= matrix.length) return;
+        if (x0 < 0 || x0 >= matrix[y0].length) return;
+
+        matrix[y0][x0] = c;
     }
 
+    /**
+     * Печатаем содержимое холста на экран.
+     */
+    void print() {
+        System.out.println();
 
-    public void print() {
-        int height = matrix.length;
-        int width = matrix[0].length;
-        String line = "";
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++){
-                line += String.valueOf(matrix[i][j]);
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
             }
-            System.out.println(line);
-            line = "";
+
+            System.out.println();
         }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
     }
 }
