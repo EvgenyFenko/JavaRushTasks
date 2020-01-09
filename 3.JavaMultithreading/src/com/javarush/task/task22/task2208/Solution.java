86610c13fba6bb6954a0a9f3cbd9f92fd4317482
efenko <evgeny@efenko.1plus1.corp>
2020-01-09 18:27:26 +0200

    2208 done

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task22.task2208;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /* 
@@ -7,9 +8,30 @@ import java.util.Map;
 */
 public class Solution {
     public static void main(String[] args) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Ivanov");
+        params.put("country", "Ukraine");
+        params.put("city", "Kiev");
+        params.put("age", null);
 
+        getQuery(params);
     }
     public static String getQuery(Map<String, String> params) {
-        return null;
+        StringBuilder result = new StringBuilder("");
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (entry.getValue() != null) {
+                if (!(result.toString().equals(""))){
+//                    result.append(entry.getValue());
+                    result.append(" and ");
+                }
+                result.append(entry.getKey());
+                result.append(" = '");
+                result.append(entry.getValue());
+                result.append("'");
+            }
+        }
+        System.out.println(result);
+
+        return result.toString();
     }
 }
