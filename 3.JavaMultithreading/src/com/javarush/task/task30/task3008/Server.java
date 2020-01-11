64539432719e75f9db415a9e2b2e932fa4d81091
efenko <evgeny@efenko.1plus1.corp>
2020-01-11 12:22:32 +0200

    3008 chat 06 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,27 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+//        ConsoleHelper.writeMessage("Input server port: ");
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Server started...");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Something wrong, Server socket closed.");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
