762a5e08c788f4519299063ce78b94b8c38e7771
efenko <evgeny@efenko.1plus1.corp>
2020-01-11 16:28:52 +0200

    3008 chat 13 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,11 +1,56 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
+    protected String getServerAddress() throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Введите адрес сервера:");
+//        return bufferedReader.readLine();
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Введите адрес порта:");
+//        return Integer.parseInt(bufferedReader.readLine());
+        ConsoleHelper.writeMessage("Введите адрес порта:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Введите имя пользователя:");
+//        return bufferedReader.readLine();
+        ConsoleHelper.writeMessage("Введите имя пользователя:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Сообщение не отправлено.");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread {
 
