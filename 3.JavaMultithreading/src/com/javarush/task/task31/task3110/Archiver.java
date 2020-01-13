90c0bbbfb8d8d80d4363b6974bf6e60a36184769
efenko <evgeny@efenko.1plus1.corp>
2020-01-13 21:09:29 +0200

    3110 (archive) part 01 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String pathToArchString = bufferedReader.readLine();
+        Path pathToArch = Paths.get(pathToArchString);
+        ZipFileManager zipFileManager = new ZipFileManager(pathToArch);
+        String pathToFileString = bufferedReader.readLine();
+        Path pathToFile = Paths.get(pathToFileString);
+        zipFileManager.createZip(pathToFile);
+    }
+}
