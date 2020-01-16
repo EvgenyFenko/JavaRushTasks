d8a677974a965d697e0d544d6549f33f89d38c4e
efenko <evgeny@efenko.1plus1.corp>
2020-01-16 11:26:21 +0200

    3110 (archive) part 16 done

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipExtractCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipExtractCommand.java
@@ -15,14 +15,14 @@ public class ZipExtractCommand extends ZipCommand {
 
             ZipFileManager zipFileManager = getZipFileManager();
 
-            ConsoleHelper.writeMessage("Введите путь для извлечения");
-            Path sourcePath = Paths.get(ConsoleHelper.readString());
-            zipFileManager.extractAll(sourcePath);
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path destinationPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(destinationPath);
 
-            ConsoleHelper.writeMessage("Архив извлечён.");
+            ConsoleHelper.writeMessage("Архив был распакован.");
 
         } catch (PathIsNotFoundException e) {
-            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+            ConsoleHelper.writeMessage("Неверный путь для распаковки.");
         }
     }
 }
