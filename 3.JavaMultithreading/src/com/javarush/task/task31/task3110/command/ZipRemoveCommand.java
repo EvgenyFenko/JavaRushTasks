be6539a6cba6989f1109e036b605a6fbcdb5d550
efenko <evgeny@efenko.1plus1.corp>
2020-01-16 11:31:24 +0200

    3110 (archive) part 17 done

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -9,14 +9,14 @@ import java.nio.file.Paths;
 public class ZipRemoveCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-            ConsoleHelper.writeMessage("Удаление архива.");
+        ConsoleHelper.writeMessage("Удаление файла из архива.");
 
-            ZipFileManager zipFileManager = getZipFileManager();
+        ZipFileManager zipFileManager = getZipFileManager();
 
-            ConsoleHelper.writeMessage("Введите полное имя объекта для удаления");
-            Path sourcePath = Paths.get(ConsoleHelper.readString());
-            zipFileManager.removeFile(sourcePath);
+        ConsoleHelper.writeMessage("Введите полный путь файла в архиве:");
+        Path sourcePath = Paths.get(ConsoleHelper.readString());
+        zipFileManager.removeFile(sourcePath);
 
-            ConsoleHelper.writeMessage("Архив создан.");
+        ConsoleHelper.writeMessage("Удаление из архива завершено.");
     }
 }
