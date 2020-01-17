70e0051cbd3d46782aba7c4c82cff7c8880393be
efenko <evgeny@efenko.1plus1.corp>
2020-01-17 10:58:19 +0200

    3209 (HTML Editor) part 14 done

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -121,4 +121,17 @@ public class View extends JFrame implements ActionListener {
     public boolean isHtmlTabSelected() {
         return tabbedPane.getSelectedIndex() == 0;
     }
+
+    public void selectHtmlTab() {
+        tabbedPane.setSelectedIndex(0);
+        resetUndo();
+    }
+
+    public void update() {
+        htmlTextPane.setDocument(controller.getDocument());
+    }
+
+    public void showAbout() {
+        JOptionPane.showMessageDialog(getContentPane(), "It hard to be God", "Information", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
