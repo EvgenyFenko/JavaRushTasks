a8732e2f7c7818a9473cf0c65be614f76f042fa5
efenko <evgeny@efenko.1plus1.corp>
2020-01-18 17:39:51 +0200

    2712 Restaurant part 07 done

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,10 +1,17 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class  AdvertisementStorage {
     private static AdvertisementStorage instance;
+    private final List<Advertisement> videos = new ArrayList<>();
 
     private AdvertisementStorage () {
-
+        Object someContent = new Object();
+        new Advertisement(someContent, "First Video", 5000, 100, 3 * 60); // 3 min
+        new Advertisement(someContent, "Second Video", 100, 10, 15 * 60); //15 min
+        new Advertisement(someContent, "Third Video", 400, 2, 10 * 60); //10 min
     }
 
     public static AdvertisementStorage getInstance() {
@@ -16,5 +23,11 @@ public class  AdvertisementStorage {
         return instance;
     }
 
+    public List<Advertisement> list() {
+        return videos;
+    }
 
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
+    }
 }
