0c5cd66dff9b1db706166740e2cc1a99e45a9007
efenko <evgeny@efenko.1plus1.corp>
2020-01-18 20:40:13 +0200

    3513 Game_2048 part 03 done

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,15 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles =  new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
