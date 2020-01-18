ded314b1fec2859dc4b4795c9dbf3bf4b5697fd5
efenko <evgeny@efenko.1plus1.corp>
2020-01-18 16:52:04 +0200

    2712 Restaurant part 06 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task27.task2712.ad;
+
+public class  AdvertisementStorage {
+    private static AdvertisementStorage instance;
+
+    private AdvertisementStorage () {
+
+    }
+
+    public static AdvertisementStorage getInstance() {
+        if (instance == null)
+            synchronized (AdvertisementStorage.class) {
+            if (instance == null)
+                instance = new AdvertisementStorage();
+            }
+        return instance;
+    }
+
+
+}
