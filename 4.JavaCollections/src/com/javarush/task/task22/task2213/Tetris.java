73074d3bc73b3ffd88f23df916a2840b5e55d7f9
efenko <evgeny@efenko.1plus1.corp>
2020-01-19 19:51:09 +0200

    2213 Tetris part 03 done

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -2,6 +2,14 @@ package com.javarush.task.task22.task2213;
 
 public class Tetris {
 
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     private Field field;
     private Figure figure;
 
