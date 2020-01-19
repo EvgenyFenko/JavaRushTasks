a0545ed1cc0de08de19873f7ac97c8c9ecdfa829
efenko <evgeny@efenko.1plus1.corp>
2020-01-19 14:06:31 +0200

    2806 done

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2301/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2301/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.task.task23.task2301;
 */
 public class Solution {
 
-    public static final class Listener {
+    public static class Listener {
         public void onMouseDown(int x, int y) {
             // Do something when the mouse down event occurs
         }
