5dad7fd340cb9b15dbdb28ccfa5b7f5f499b11a6
efenko <evgeny@efenko.1plus1.corp>
2020-01-19 16:47:17 +0200

    2028 Tree part 03 done

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -10,6 +10,31 @@ import java.util.List;
 */
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
 
+    static class Entry<T> implements Serializable {
+        String elementName;
+        int lineNumber;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        void checkChildren() {
+            if (leftChild != null)
+                availableToAddLeftChildren = false;
+            if (rightChild != null)
+                availableToAddRightChildren = false;
+        }
+
+        boolean isAvailableToAddChildren() {
+            return availableToAddLeftChildren || availableToAddRightChildren;
+        }
+    }
+
+
     @Override
     public int size() {
         return 0;
