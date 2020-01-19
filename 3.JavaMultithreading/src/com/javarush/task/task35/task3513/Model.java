a61930ec66e44a8c4b319ff0f6643ca512ea699a
efenko <evgeny@efenko.1plus1.corp>
2020-01-19 14:35:23 +0200

    3513 Game_2048 part 05 done

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    int score;
+    int maxTile;
 
     public Model() {
         resetGameTiles();
+        score = 0;
+        maxTile = 0;
     }
 
     private List<Tile> getEmptyTiles() {
@@ -39,6 +43,40 @@ public class Model {
         }
         addTile();
         addTile();
+    }
+
+    private boolean mergeTiles(Tile[] tiles) {
+        boolean isChanged = false;
+        for (int i = 1; i < tiles.length; i++) {
+            if (tiles[i].value != 0 && tiles[i - 1].value == tiles[i].value) {
+                isChanged = true;
+                tiles[i - 1].value = tiles[i - 1].value * 2;
+                for (int j = i + 1; j < tiles.length; j++) {
+                    tiles[j - 1].value = tiles[j].value;
+                }
+                tiles[tiles.length - 1].value = 0;
+
+                score += tiles[i - 1].value;
+                if (tiles[i - 1].value > maxTile) maxTile = tiles[i - 1].value;
+            }
+        }
+        return isChanged;
+    }
+
+    private boolean compressTiles(Tile[] tiles) {
+        boolean isChanged = false;
+        for (int i = 1; i < tiles.length; i++) {
+            int index = i;
+            while (index != 0 && tiles[index - 1].isEmpty() && !tiles[i].isEmpty()) {
+                index--;
+            }
+            if (index != i) {
+                tiles[index].value = tiles[i].value;
+                tiles[i].value = 0;
+                isChanged = true;
+            }
+        }
+        return isChanged;
     }
 
 }
