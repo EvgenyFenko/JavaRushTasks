0f7db6b4889eb299e5d5948ffd75209a3071ae57
efenko <evgeny@efenko.1plus1.corp>
2020-01-20 12:18:32 +0200

    2515 Space part 13 done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -15,4 +15,20 @@ public class SpaceShip extends BaseObject {
     public void moveRight() {
         dx = 1;
     }
+
+    @Override
+    public void move() {
+        x += dx;
+
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() + 1);
+    }
+
+    public void draw(Canvas canvas) {
+
+    }
+
+    public void fire() {
+        Space.game.getRockets().add(new Rocket(x - 2, y));
+        Space.game.getRockets().add(new Rocket(x + 2, y));
+    }
 }
